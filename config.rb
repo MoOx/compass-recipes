032f13fd70f521c68babb863a4072c11d328edb1
Maxime Thirouin <maxime.thirouin@gmail.com>
2011-06-17 09:24:41 +0200

    Update for gem directory structure

--- config.rb
+++ config.rb
@@ -1,6 +1,13 @@
+##
+# Compass Recipes
+# Configuration files mainly used for tests
+#
 http_path = "/"
 css_dir = "."
 sass_dir = "."
-add_import_path "./src"
-add_import_path "./tests"
 line_comments = false
+
+additional_import_paths = [
+    "./stylesheets",
+    "./tests"
+]
