941ab21f881b8273014a65b0258654a3b5777946
Maxime Thirouin <maxime.thirouin@gmail.com>
2011-11-24 03:32:48 +0100

    New background Noise + background tests

--- config.rb
+++ config.rb
@@ -11,3 +11,10 @@ additional_import_paths = [
     "./stylesheets",
     "./tests"
 ]
+
+# just for testing extension
+require File.join(File.dirname(__FILE__), 'lib', 'compass-recipes')
+
+# for repeating-linear-gradient
+# https://github.com/chriseppstein/compass/issues/401
+Compass::BrowserSupport.add_support("repeating-linear-gradient", "webkit", "moz", "o", "ms")
