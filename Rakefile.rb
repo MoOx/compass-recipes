4a814b0ec16257104cc12a518c4148e6d9adf68d
Maxime Thirouin <maxime.thirouin@gmail.com>
2012-03-28 08:27:18 +0200

    Update build demos/tests gh-pages script, close #30

--- Rakefile.rb
+++ Rakefile.rb
@@ -10,32 +10,47 @@ task :pages do
   #  copy tests into a temp dir before switching branch
   FileUtils.rm_rf "tmp"
   FileUtils.mkdir("tmp")
-  (FileList.new('tests/**/*.html')+FileList.new('tests/**/*.css')).each do |file|
-    FileUtils.mkdir_p(File.dirname("tmp/#{file[6..-1]}"))
-    FileUtils.cp(file, "tmp/#{file[6..-1]}")
+  (FileList.new('fonts/**/*')+FileList.new('stylesheets/**/*')+FileList.new('templates/**/*')+FileList.new('tests/**/*')).each do |file|
+    if File.file?(file)
+      puts 'File ' + file
+      if !File.directory?(File.dirname("tmp/#{file}"))
+        FileUtils.mkdir_p(File.dirname("tmp/#{file}"))
+      end
+      FileUtils.cp(file, "tmp/#{file}")
+    else #assume dir
+      puts 'Dir  ' + file
+      FileUtils.mkdir_p("tmp/#{file}")
+    end
   end
 
   # switch branch
   repo.branch("gh-pages").checkout
 
-  # Prepare gh-pages
-  FileUtils.rm_rf "recipes/*"
-  htmlHeader = File.open("layout/header.html", "r").read
+  # Reset gh-pages
+  FileUtils.rm_rf "fonts/*"
+  FileUtils.rm_rf "stylesheets/*"
+  FileUtils.rm_rf "templates/*"
+  FileUtils.rm_rf "tests/*"
+
+  # Prepare tests addons
+  headMarker = '<!doctype html>'
+  htmlHeader = headMarker + File.open("layout/header.html", "r").read
   htmlFooter = File.open("layout/footer.html", "r").read
 
   # HTML files need header and footer
   FileList.new('tmp/**/*.html').each do |file|
-    FileUtils.mkdir_p(File.dirname("#{file[4..-1]}"))
-    htmlfile = File.open("#{file[4..-1]}", "w")
     contents = File.open(file, "rb").read
-    htmlfile.write(htmlHeader+contents+htmlFooter)
+    htmlfile = File.open("#{file}", "w")
+    contents = contents.sub(headMarker, htmlHeader)
+    htmlFooter += contents
+    htmlfile.write(contents)
     htmlfile.close
   end
 
   # CSS: just copy
-  FileList.new('tmp/**/*.css').each do |file|
+  FileList.new('tmp/*').each do |file|
     FileUtils.mkdir_p(File.dirname("#{file[4..-1]}"))
-    FileUtils.cp(file, "#{file[4..-1]}")
+    FileUtils.mv(file, "#{file[4..-1]}")
   end
 
   FileUtils.rm_rf("tmp")
@@ -45,8 +60,8 @@ task :pages do
   Dir["recipes/**/*"].each {|f| repo.add(f) }
   repo.status.deleted.each {|f, s| repo.remove(f)}
   message = ENV["MESSAGE"] || "Updated at #{Time.now.utc}"
-  repo.commit(message)
+  #repo.commit(message)
 
   # back to master (maybe it's not appropriate if we are not working on master ?!)
-  repo.branch("master").checkout
+  #repo.branch("master").checkout
 end
