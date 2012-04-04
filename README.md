3cb7440bdfee773fa9d8312decdd6c3eb25d4e19
Maxime Thirouin <maxime.thirouin@gmail.com>
2012-04-04 08:52:32 +0200

    Update with latest gh-pages changes

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Compass Recipes ![project status](http://stillmaintained.com/MoOx/compass-recipes.png) #
+# Compass Recipes
 
 This project is a collection of Sass mixins using Compass that you can use or learn from.
 
 This git branch is for web pages only.
+
 See Master branch https://github.com/MoOx/compass-recipes
--- config.rb
+++ config.rb
@@ -1,7 +1,24 @@
-http_path = "/"
-css_dir = "."
-sass_dir = "."
-images_dir = "img"
-#javascripts_dir = "js"
+##
+# Compass Recipes
+# Configuration files mainly used for tests
+#
 
-output_style = :compressed
+# for testing extension
+require File.join(File.dirname(__FILE__), 'lib', 'compass-recipes')
+
+http_path = '/'
+css_dir = '.'
+sass_dir = '.'
+images_dir = 'img'
+
+line_comments = false
+relative_assets = true
+
+additional_import_paths = [
+    './stylesheets',
+    './tests'
+]
+
+# for repeating-linear-gradient
+# https://github.com/chriseppstein/compass/issues/401
+Compass::BrowserSupport.add_support('repeating-linear-gradient', 'webkit', 'moz', 'o', 'ms')
