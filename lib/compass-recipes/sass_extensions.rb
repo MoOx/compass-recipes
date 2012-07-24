2edf1840ac78198330009fbe31f5785017922762
MoOx <m@moox.fr>
2012-07-24 08:28:18 +0200

    Delete sqrt() & pow() (they are in Compass already).

--- lib/compass-recipes/sass_extensions.rb
+++ lib/compass-recipes/sass_extensions.rb
@@ -2,7 +2,7 @@
 # Sass extensions for Compass Recipes
 ##
 
-%w(_error background_noise gravatar math).each do |lib|
+%w(_error background_noise gravatar).each do |lib|
   require File.join(File.dirname(__FILE__), 'sass_extensions', lib)
 end
 
--- lib/compass-recipes/sass_extensions/math.rb
+++
@@ -1,8 +0,0 @@
-module Sass::Script::Functions
-    def pow(number, power)
-        Sass::Script::Number.new(number.value ** power.value)
-    end
-    def sqrt(number)
-        Sass::Script::Number.new(Math.sqrt(number.value))
-    end
-end
